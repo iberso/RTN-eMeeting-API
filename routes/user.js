7ab5d1cf892598f72e37cdf5115869038e6dc385
iverson <c14190189@john.petra.ac.id>
2022-10-10 12:44:48 +0700

    minor

--- routes/user.js
+++ routes/user.js
@@ -33,7 +33,7 @@ module.exports = {
         try {
             const data = await pool.query(sql, value);
             if (data.length != 0) {
-                console.log(data[0].profile_picture_path.length)
+                console.log(data[0].profile_picture_path)
                 if (data[0].profile_picture_path != null) {
                     return helper.http_response(data[0].profile_picture_path, 'success', null);
                 } else {
