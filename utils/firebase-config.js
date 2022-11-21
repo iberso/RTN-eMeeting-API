e07124f0de9ccd3ce7b917fd43673e0058012da5
iverson <c14190189@john.petra.ac.id>
2022-11-21 16:11:38 +0700

    remove firebase credent

--- utils/firebase-config.js
+++ utils/firebase-config.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
+
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../files/emeeting-e4bd6-firebase-adminsdk-cq674-53f62fa51d.json');
-
 // https://stackoverflow.com/questions/46901556/firebase-error-messaging-authentication-error
+const serviceAccount = JSON.parse(process.env['FIREBASE_CREDENTIALS']);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
